2c2e5ca8e037319ed1893f2261660437c6f1f978
tech7895 <techhunter990@gmail.com>
2023-10-15 03:29:32 -0700

    string update

--- strings.c
+++ strings.c
@@ -1,27 +1,61 @@
 #include "s_shell.h"
+
 /**
- * _strlen - This function returns the length of a string
- * @s: Input string
- * Return: the length of the string
+ * _strlen - This function  returns the length of string
+ * @s: the string
+ *
+ * Return: the length
+ *
  */
 int _strlen(char *s)
 {
-	int s_len = 0;
+	int s_len;
+
+	s_len = 0;
 
 	while (s[s_len] != '\0')
 	{
-		s_len++;
+	s_len++;
 	}
 
 	return (s_len);
 }
 
 /**
+ * _strcat - This function concertenates two strings
+ * @dest: the destination
+ * @src: the source
+ *
+ * Return: the concatinated string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int l1, l2, i;
+
+	for (l1 = 0; dest[l1] != '\0'; l1++)
+	{
+
+	}
+
+	for (l2 = 0; src[l2] != '\0'; l2++)
+	{
+
+	}
+	for (i = 0; i <= l2; i++)
+	{
+		dest[l1 + i] = src[i];
+	}
+
+	return (dest);
+}
+
+/**
  * *_strcpy - This function copies a string
  *
  * @dest: the destination
- * @src: the source string
- * Return: the pointer (dest)
+ * @src: the source
+ *
+ * Return: dest
  */
 char *_strcpy(char *dest, char *src)
 {
@@ -32,49 +66,28 @@ char *_strcpy(char *dest, char *src)
 		dest[i] = src[i];
 	}
 	dest[i] = '\0';
+
 	return (dest);
 }
 
 /**
  * _strcmp - This function compares two strings
+ *
  * @s1: the first string
  * @s2: the second string
  *
- * Return: 0 if the strings are the same, non-zero if not
+ * Return: the difference
+ *
  */
 int _strcmp(char *s1, char *s2)
 {
 	int i;
-	int comp = 0;
+	int d = 0;
 
-	for (i = 0; s1[i] != '\0' && comp == 0; i++)
+	for (i = 0; s1[i] != '\0' && d == 0; i++)
 	{
-		comp = s1[i] - s2[i];
+		d = s1[i] - s2[i];
 	}
 
-	return (comp);
-}
-
-/**
- * _strcat - This function concertenates two strings
- *
- * @dest: the destination string
- * @src: the source string
- *
- * Return: the destination string
- */
-char *_strcat(char *dest, char *src)
-{
-	int i, lend = 0;
-
-	for (i = 0; dest[i] != '\0'; i++)
-	{
-		lend++;
-	}
-
-	for (i = 0; src[i] != '\0'; i++)
-	{
-		dest[lend + i] = src[i];
-	}
-	return (dest);
+	return (d);
 }
