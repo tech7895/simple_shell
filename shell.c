3b6147d05b9e025a5ac83b6668c6319889288b4b
tech7895 <techhunter990@gmail.com>
2023-10-15 03:48:22 -0700

    main function

---
+++ shell.c
@@ -0,0 +1,36 @@
+#include "s_shell.h"
+/**
+ * main - This function is the main program
+ * Description: this runs a basic shell
+ * @ac: argument count
+ * @av: arguments vector
+ *
+ * Return: the cond
+ *
+ **/
+int main(int ac, char **av)
+{
+	int cond;
+	b_shell_t *b_type;
+
+	b_type = malloc(sizeof(b_shell_t));
+	if (b_type == NULL)
+	{
+		perror(av[0]);
+		exit(1);
+	}
+
+	b_type->pid = getpid();
+	b_type->code_stat = 0;
+	b_type->n_cmd = 0;
+	b_type->argc = ac;
+	b_type->argv = av;
+	b_type->it_mode = isatty(0) == 1;
+	begin(b_type);
+
+	cond = b_type->code_stat;
+
+	free(b_type);
+
+	return (cond);
+}
